8c5862a71edaa0f7345908745b2bd6b870cb3a98
eagleheartangel <eagleheartangel88@gmail.com>
2021-02-22 02:51:57 -0600

    header

--- app.js
+++ app.js
@@ -3,8 +3,17 @@
 window.addEventListener('scroll', function () {
   var header = document.querySelector('nav');
   var link = document.querySelectorAll('#link');
+  var menunegro = document.querySelectorAll('#menunegro');
+  var menublanco = document.querySelectorAll('#menublanco');
   header.classList.toggle('sticky', window.scrollY > 0);
   for (var i = 0; i < link.length; i++) {
     link[i].classList.toggle('sticky', window.scrollY > 0);
+    if (window.scrollY > 0) {
+      menunegro[0].classList.add('d-none');
+      menublanco[0].classList.remove('d-none');
+    } else {
+      menunegro[0].classList.remove('d-none');
+      menublanco[0].classList.add('d-none');
+    }
   }
 });
